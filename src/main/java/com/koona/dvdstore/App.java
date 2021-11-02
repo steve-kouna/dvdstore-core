a0adbd7a7817df8509356f80a57db9a448338e0b
Steve KOUNA <steve.kouna@afrinic.net>
2021-11-02 11:40:48 +0400

    save

--- src/main/java/com/koona/dvdstore/App.java
+++ src/main/java/com/koona/dvdstore/App.java
@@ -1,23 +1,30 @@
 package com.koona.dvdstore;
 
 import com.koona.dvdstore.controller.MovieController;
-import com.koona.dvdstore.repository.file.FileMovieRepository;
-import com.koona.dvdstore.service.DefaultMovieService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {
+    "com.koona.dvdstore.controller",
+    "com.koona.dvdstore.service",
+    "com.koona.dvdstore.repository.file"
+})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {
 //        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
     }
