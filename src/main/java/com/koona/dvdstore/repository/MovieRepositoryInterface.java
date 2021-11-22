45febff8996e152e8f4400f9c66bb0904bc1e58c
Steve KOUNA <stevekouna@gmail.com>
2021-11-22 20:35:23 +0400

    tp 35 et 36

--- src/main/java/com/koona/dvdstore/repository/MovieRepositoryInterface.java
+++ src/main/java/com/koona/dvdstore/repository/MovieRepositoryInterface.java
@@ -6,15 +6,19 @@
 package com.koona.dvdstore.repository;
 
 import com.koona.dvdstore.entity.Movie;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  *
  * @author Steve KOUNA
  */
 public interface MovieRepositoryInterface extends CrudRepository<Movie, Long> {
+    @EntityGraph(value = "movie.actor-and-reviews", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<Movie> findById(Long id);
     /*
     Movie add (Movie movie);
     List<Movie> list();
